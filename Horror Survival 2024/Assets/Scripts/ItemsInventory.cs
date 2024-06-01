b47725dd235e01dede40fb52f0d2dcb708aca1e6
Pablo Miranda <pablo.hbmiranda@gmail.com>
2024-06-01 14:06:18 -0300

    :sparkles: feat: add combine UI
    Adiciona UI para combinar itens

--- Horror Survival 2024/Assets/Scripts/ItemsInventory.cs
+++ Horror Survival 2024/Assets/Scripts/ItemsInventory.cs
@@ -12,6 +12,7 @@ public class ItemsInventory : MonoBehaviour
     public string[] descriptions;
     public Text description;
     public Button[] itemButtons;
+    public GameObject useButton;
 
     private AudioSource audioPlayer;
     public AudioClip click, select;
@@ -25,6 +26,7 @@ public class ItemsInventory : MonoBehaviour
         bigIcon.sprite = bigIcons[0];
         title.text = titles[0];
         description.text = descriptions[0];
+        useButton.SetActive(false);
     }
 
     private void OnEnable()
@@ -52,6 +54,9 @@ public class ItemsInventory : MonoBehaviour
         audioPlayer.clip = click;
         audioPlayer.Play();
         chosenItemNumber = itemNumber;
+
+        if(itemNumber < 4) { useButton.SetActive(false); }
+        else { useButton.SetActive(true);}
     }
 
     public void AssignItem()

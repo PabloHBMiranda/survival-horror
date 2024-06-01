6b6767fafb8d123527d77d33c63385fec9125f0c
Pablo Miranda <pablo.hbmiranda@gmail.com>
2024-06-01 09:20:19 -0300

    :sparkles: feat: add sound click inventory
    Adiciona som de seleção dos itens no inventário.

--- Horror Survival 2024/Assets/Scripts/WeaponInventory.cs
+++ Horror Survival 2024/Assets/Scripts/WeaponInventory.cs
@@ -11,10 +11,18 @@ public class WeaponInventory : MonoBehaviour
     public Text title;
     public string[] descriptions;
     public Text description;
+
+    private AudioSource audioPlayer;
+    public AudioClip click, select;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioPlayer = GetComponent<AudioSource>();
+
+        bigIcon.sprite = bigIcons[0];
+        title.text = titles[0];
+        description.text = descriptions[0];
     }
 
     // Update is called once per frame
@@ -28,5 +36,7 @@ public class WeaponInventory : MonoBehaviour
         bigIcon.sprite = bigIcons[weaponNumber];
         title.text = titles[weaponNumber];
         description.text = descriptions[weaponNumber];
+        audioPlayer.clip = click;
+        audioPlayer.Play();
     }
 }
